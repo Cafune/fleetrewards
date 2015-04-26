0edf3375bc5855eb3b520a84b71d140d1446f9c1
kalam <chris@m6u.net>
2015-04-26 13:31:57 -0700

    fixed body css so it's not carried over from login

--- client/js/login.js
+++ client/js/login.js
@@ -1,5 +1,13 @@
 // login.js
 
+Template.login.onRendered(function () {
+  document.body.className='loginScreen';
+});
+
+Template.login.onDestroyed(function () {
+  document.body.className='';
+});
+
 Template.login.events({
   "click #login": function (event, template) {
     Meteor.loginWithEve();
