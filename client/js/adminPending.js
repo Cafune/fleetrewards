8ac6c9afc9d6e463e1dc9f0465375f1f17198579
kalam <chris@m6u.net>
2015-04-28 17:29:42 -0700

    admin pending changes

--- client/js/adminPending.js
+++ client/js/adminPending.js
@@ -1,8 +1,8 @@
 // admin.js
 
 Template.pendingAdminFleets.helpers({
-  fleets: function () {
-    var fleets = Fleets.find({}, {sort: {modified: -1}}).fetch();
+  pendingFleets: function () {
+    var fleets = Fleets.find({status: 'Pending'}, {sort: {modified: -1}}).fetch();
     for (var i = 0; i < fleets.length; i++) {
       var user = Meteor.users.findOne(fleets[i].user_id);
       fleets[i].user_name = user && user.profile && user.profile.name;;
@@ -23,8 +23,8 @@ Template.pendingAdminFleets.helpers({
 });*/
 
 Template.pendingAdminPayouts.helpers({
-  payouts: function () {
-    var payouts = Payouts.find({}, {sort: {modified: -1}}).fetch();
+  pendingPayouts: function () {
+    var payouts = Payouts.find({status: 'Pending'}, {sort: {modified: -1}}).fetch();
     for (var i = 0; i < payouts.length; i++) {
       var user = Meteor.users.findOne(payouts[i].user_id);
       payouts[i].user_name = user.profile.name;
@@ -34,5 +34,33 @@ Template.pendingAdminPayouts.helpers({
       payouts[i].isk_cost = numeral(payouts[i].isk_cost).format('0,0.00');
     }
     return payouts;
+  }
+});
+
+Template.pendingAdminFleets.events({
+  'click .btn': function (event, template) {
+    $('#fleetId').val(this._id);
+    $('#userName').text(this.user_name);
+    $('#fleetName').val(this.fleet_name);
+    $('#papLink').val(this.pap_link);
+    $('#ping').val(this.ping);
+    $('#additionalInformation').val(this.additional_information);
+  }
+});
+
+Template.modal.events({
+  'submit form': function (event, template) {
+
+    console.log(this._id);
+    console.log('test');
+    /*var userId = Meteor.userId();
+    var fleetName = event.target.fleetName.value;
+    var papLink = event.target.papLink.value;
+    var ping = event.target.ping.value;
+    var additionalInformation = event.target.additionalInformation.value;
+    var status = 'Pending';
+    var date = new Date(); // current date
+    */
+    return false;
   }
 });
