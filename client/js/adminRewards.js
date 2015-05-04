62582d0b3cccadd5b898ade4e4f837ea09f26c9e
Adam Trager <unonymous253@gmail.com>
2015-05-04 13:23:36 -0400

    Changed ,method for renaming reward type

--- client/js/adminRewards.js
+++ client/js/adminRewards.js
@@ -1,14 +1,3 @@
 Template.adminRewards.onRendered(function () {
   $('[data-toggle="tooltip"]').tooltip() //initialize all tooltips in this template
 });
-
-Template.rewardGroup.events({
-  'click .fa-pencil' : function(event, template) {
-    $('#typeEdit').show();
-    $('#typeName').hide();
-  },
-  'click #changeName' : function(event, template) {
-    $('#typeEdit').hide();
-    $('#typeName').show();
-  }
-})
